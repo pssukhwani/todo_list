d03d35a7f9bd87f1199c5590d8144d5daa9a9b36
PS <ps.sukhwani@gmail.com>
2018-11-14 23:14:15 +0530

    order_by due date ascending order

--- api/views.py
+++ api/views.py
@@ -7,10 +7,7 @@ from core.models import Task
 def home_page(request):
     data = {"api_name": "todo"}
     if request.user.is_authenticated:
-        task = Task.objects.filter(user=request.user, is_active=True)
-        if request.GET.get("search"):
-            task = task.filter(title__icontains=request.GET.get("search"))
-            data["success"] = True
+        task = Task.objects.filter(user=request.user, is_active=True).order_by("due_date")
         data["task"] = task
         return render(request, "task.html", data)
     return render(request, "login_registration.html", data)
