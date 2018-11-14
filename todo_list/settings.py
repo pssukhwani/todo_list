83aeac21cbd2b760ee26e2835bb44fa45cb5a1c9
PS <ps.sukhwani@gmail.com>
2018-11-14 10:26:06 +0530

    settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rz-jhdg4e+_pkn-6l)@4oj961w$03p!*3*_-%mt&-p(019ghb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
