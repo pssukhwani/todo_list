c54e4dde2173d8aa865b96fd60d459bb0139614b
PS <ps.sukhwani@gmail.com>
2018-11-16 11:12:16 +0530

    minor changes - test

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -50,16 +50,3 @@ class TestMyView(object):
                         data={"title": "test2", "description": "test2", "user": self.create_user(), "testing": True,
                               "datetime": datetime.today().strftime("%d/%m/%Y %H:%M:%S"), "set_alert": 3})
         assert response.status_code == 200
-
-    # @pytest.mark.django_db(transaction=True)
-    # def test_task_update(self):
-    #     update_response = post(self.get_hostname() + reverse("api_task_update", kwargs={"api_name": "todo"}),
-    #                            data={"id": self.create_task().id, "title": "test2", "description": "test2",
-    #                                  "due_date": datetime.today(), "set_alert": 3})
-    #     delete_reponse = post(self.get_hostname() + reverse("api_task_update", kwargs={"api_name": "todo"}),
-    #                           data={"delete": self.create_task().id})
-    #     check_response = post(self.get_hostname() + reverse("api_task_update", kwargs={"api_name": "todo"}),
-    #                           data={"checked": self.create_task().id})
-    #     assert delete_reponse.status_code == 200
-    #     assert check_response.status_code == 200
-    #     assert update_response.status_code == 200
